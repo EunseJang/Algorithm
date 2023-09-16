bc391d48e78025e0df06aa935ee6b258b02564d6
Eunse Jang <senuej37@gmail.com>
2023-09-16 09:39:48 +0900

    feat: 퀵정렬

--- data-structure/sorting/quick_sort.cpp
+++ data-structure/sorting/quick_sort.cpp
@@ -8,10 +8,11 @@ void Swap(int &a, int &b) {
   b = temp;
 }
 
+// 피벗(pivot) 위치 구하기
 int Partition(int A[], int left, int right) {
-  int partElem, value;
-  partElem = left;
-  value = A[partElem];
+  int pivot, value;
+  pivot = left;
+  value = A[pivot];
 
   do {
     while (A[++left] < value);
@@ -24,17 +25,17 @@ int Partition(int A[], int left, int right) {
     }
   } while (1);
 
-  A[partElem] = A[right];
+  A[pivot] = A[right];
   A[right] = value;
 
-  return right; // partition값의 index
+  return right;
 }
 
 void QuickSort(int A[], int left, int right) {
   int k;
 
   if (left < right) {
-    k = Partition(A, left, right+1); // 전체 배열 범위 (dummy값 포함)
+    k = Partition(A, left, right+1);
     QuickSort(A, left, k-1);
     QuickSort(A, k+1, right);
   }
@@ -45,7 +46,7 @@ int main () {
   int A[] = {30, 20, 40, 35, 5, 10, 45, 50, 25, 15, dummy};
   int size = sizeof(A)/sizeof(A[0]);
 
-  QuickSort(A, 0, size-1); // dummy값 제외
+  QuickSort(A, 0, size-1);
 
   for (int i=0; i<size-1; i++) {
     cout << A[i] << " ";
